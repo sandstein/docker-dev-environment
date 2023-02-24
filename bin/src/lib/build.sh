d4bb7649117478a29ed0a349ca7d864ff009cc02
Wilfried Wolf <wilfried.wolf@sandstein.de>
2023-02-24 17:21:15 +0100

    prepares everything for nginx

--- bin/src/lib/build.sh
+++ bin/src/lib/build.sh
@@ -1,8 +1,7 @@
 touchConfigFile () {
 
   if [[ $1 =~ "php" ]]; then
-    part="${1/-x//}"
-    configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$part/etc/php/php.ini"
+    configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/php/php.ini"
   fi
 
   if [[ $1 =~ mysql|percona|mariadb ]]; then
@@ -13,13 +12,31 @@ touchConfigFile () {
     configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/tunnel.conf"
   fi
 
+  if [[ $1 =~ "nginx" ]]; then
+      configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/nginx/conf.d/default.conf"
+  fi
+
+  if [[ $1 =~ "varnish" ]]; then
+    configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/default.vcl"
+  fi
+
   if [[ -n "${configFile}" && ! -f "${configFile}" ]]; then
     echo "Touching ${configFile} before startup, because it does not exist"
     touch "${configFile}"
-    if [[ $part =~ "php" ]]; then
+    if [[ $1 =~ "php" ]]; then
       echo 'Copying ini files, adopt to your needs'
-      cp "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$part/sample/conf.d/*.ini" \
-         "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$part/etc/php/conf.d"
+      cp "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/conf.d/*.ini" \
+         "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/php/conf.d"
+    fi
+    if [[ $1 =~ "nginx" ]]; then
+        echo 'Copying config files, adopt to your needs'
+        cp -R "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/conf.d/default.conf" \
+            "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/nginx/conf.d"
+    fi
+    if [[ $1 =~ "varnish" ]]; then
+        echo 'Copying config files, adopt to your needs'
+        cp "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/default.vlc" \
+            "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/varnish/"
     fi
   fi
 }
