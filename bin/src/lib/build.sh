eb3b53ee9423f4f1a4023c834c01f1a3be0c9619
Wilfried Wolf <wilfried.wolf@sandstein.de>
2023-03-22 09:00:56 +0100

    adds config skeleton for nginx

--- bin/src/lib/build.sh
+++ bin/src/lib/build.sh
@@ -17,26 +17,25 @@ touchConfigFile () {
   fi
 
   if [[ $1 =~ "varnish" ]]; then
-    configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/default.vcl"
+    configFile="${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/varnish/default.vcl"
   fi
 
   if [[ -n "${configFile}" && ! -f "${configFile}" ]]; then
-    echo "Touching ${configFile} before startup, because it does not exist"
-    touch "${configFile}"
     if [[ $1 =~ "php" ]]; then
       echo 'Copying ini files, adopt to your needs'
       cp "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/conf.d/*.ini" \
          "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/php/conf.d"
-    fi
-    if [[ $1 =~ "nginx" ]]; then
-        echo 'Copying config files, adopt to your needs'
+    elif [[ $1 =~ "nginx" ]]; then
+        echo 'Copying config file, adopt to your needs'
         cp -R "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/conf.d/default.conf" \
             "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/nginx/conf.d"
-    fi
-    if [[ $1 =~ "varnish" ]]; then
-        echo 'Copying config files, adopt to your needs'
-        cp "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/default.vlc" \
+    elif [[ $1 =~ "varnish" ]]; then
+        echo 'Copying config file, adopt to your needs'
+        cp "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/sample/default.vcl" \
             "${DOCKER_DEV_ENVIRONMENT_HOME}/config/$1/etc/varnish/"
+    else
+        echo "Touching ${configFile} before startup, because it does not exist"
+        touch "${configFile}"
     fi
   fi
 }
